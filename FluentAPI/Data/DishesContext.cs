2d348ff89a07d6efd553626b490cbe01b62f94c9
Bola_sw25@student.itstep.kz <Bola_sw25@student.itstep.kz>
2021-03-23 21:32:53 +0600

    Добавьте файлы проекта.

---
+++ FluentAPI/Data/DishesContext.cs
@@ -0,0 +1,63 @@
+﻿using FluentAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentAPI.Data
+{
+    public class DishesContext : DbContext
+    {
+        public DishesContext()
+        {
+            Database.Migrate();
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Dish> Dishes { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = (localdb)\\mssqllocaldb; Database = FluentAPI; Trusted_Connection = true;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .ToTable("products")
+                .Property(product => product.Id)
+                .HasColumnName("ID");
+
+            modelBuilder.Entity<Product>()
+                .HasKey(product => product.Id);
+               
+            modelBuilder.Entity<Product>()
+                .Property(product => product.Name)
+                .HasColumnName("name")
+                .HasMaxLength(50)
+                .IsRequired();
+
+            modelBuilder.Entity<Product>()
+               .Property(product => product.DishId)
+               .HasColumnName("dishId");
+
+            modelBuilder.Entity<Product>()
+                .HasOne(product => product.Dish)
+                .WithMany(dish => dish.Products);
+
+            modelBuilder.Entity<Dish>()
+               .ToTable("dishes")
+               .Property(dish => dish.Id)
+               .HasColumnName("ID");
+
+            modelBuilder.Entity<Dish>()
+                .HasKey(dish => dish.Id);
+
+            modelBuilder.Entity<Dish>()
+                .Property(dish => dish.Name)
+                .HasColumnName("name")
+                .HasMaxLength(50)
+                .IsRequired();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ FluentAPI/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentAPI.Models
+{
+    public class Product : Entity
+    {
+        public string Name { get; set; }
+        public Guid DishId { get; set; }
+        public Dish Dish { get; set; }
+    }
+}
